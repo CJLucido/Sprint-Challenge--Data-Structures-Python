767cfc4a5cdc18439e4c89110acdead3b24dcd9a
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-27 11:14:22 -0700

    got through the empty list test

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -47,4 +47,53 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         # You must use recursion for this solution
-        pass
+        if not node:
+            return
+        else:
+            next_node_to_switch_places = node.get_next()
+            print(next_node_to_switch_places.value)
+            if not next_node_to_switch_places:
+                return
+            else:
+                new_prev = next_node_to_switch_places.get_next()
+
+            if prev == None:
+                node.set_next(None)
+                
+            else:
+                node.set_next(prev)
+            
+
+            if not new_prev:
+                return
+            else:
+                self.reverse_list(next_node_to_switch_places, new_prev)      
+            
+
+
+#Plan--------------------------------------------------------------
+
+# capture the next
+# next_node_to_switch_places = head.next
+# head.prev = head.next
+# head.next = head.prev (or none) #if/conditional
+# recursive(next_node_to_switch_places)
+
+# runthrough:
+# next_node_to_switch_places = 2
+# head.prev = 2
+# head.next = none
+# recursive(next_node_to_switch_places/2)
+# 2 1 none 3 none
+# next_node_to_switch_places = 3
+# head.prev = 3
+# head.next = 1
+# recursive(next_node_to_switch_places/3)
+# 3 none 2 1 none
+# next_node_to_switch_places = none #if/conditional, once this happens don't call the recursive function
+# head.prev = none (conditional?)
+# head.next = 2
+# if next_node_to_switch_places == None:
+# 	return
+# else:
+# 	recursive(next_node_to_switch_places/3)
