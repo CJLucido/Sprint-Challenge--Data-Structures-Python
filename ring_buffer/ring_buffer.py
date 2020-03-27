627a21ba6183b4b68225819f6bc644f73aeb7d72
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-27 08:42:27 -0700

    passing 7 tests, need to add cursor somehow

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,15 +6,37 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        #self.buffer ={} unnecessary
 
     def append(self, item):
-        pass
+        #key = self.storage.length
+        if self.capacity== self.storage.length:
+            self.storage.remove_from_head() #remove oldest
+            self.storage.add_to_head(item)
+            #key = capacity - 1 #unnecessary now
+            #self.buffer[key] = value #overwrite the old # actually this is just redo'ing array functionality but worse
+        elif self.capacity > self.storage.length:
+            self.storage.add_to_tail(item) #this will add it to the head too if no head
+            #self.buffer[key] = value
+        #self.length += 1 unnecessary
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+
+        self.current = self.storage.head
+        i = 0
+        for i in range(self.storage.length):
+            if self.current:
+                list_buffer_contents.append(self.current.value)
+                self.current = self.current.next
+            else:
+                continue
+            i += 1
+
 
         return list_buffer_contents
 
